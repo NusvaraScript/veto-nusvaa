7ea42c3e3e5ad4ab8bb05a5fe77b9df32b4e89e1
Nusvara <yusufusman11111111@gmail.com>
2026-04-18 23:00:24 +0700

    update: footer and navbar

--- resources/views/layout/component/navbar.blade.php
+++ resources/views/layout/component/navbar.blade.php
@@ -1,5 +1,9 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-black border-bottom border py-3">
-    <div class="container">
-        <a href="" class="navbar-brand"></a>
+<nav class="bg-white border-b-2 border-black sticky top-0 z-50">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-12">
+            <div class="flex items-center">
+                <a href="{{ route('home') }}" class="text-2xl font-bold text-black">Veto<span class="text-red-700">Nusvaa</span>.</a>
+            </div>
+        </div>
     </div>
 </nav>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 @section('title', 'Beranda - VetoNusvaa')
 @section('content')
-    <main>
+    <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
     </main>
 @endsection
