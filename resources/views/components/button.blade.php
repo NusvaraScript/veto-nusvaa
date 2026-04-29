6b34ed5a738dc25fe1d0e78b7d311df12ad09ffa
Nusvara <yusufusman11111111@gmail.com>
2026-04-29 13:13:33 +0700

    fix the web design

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -8,11 +8,11 @@
 
 @php
     $button = match($variant) {
-        'solid' => 'inline-block bg-red-600 text-white border-2 border-black px-4 py-1 font-bold uppercase hover:bg-white hover:text-black hover:shadow-block hover:scale-105 active:scale-95 transition-all',
-        'outline' => 'inline-block bg-white text-black border-2 border-black px-4 py-1 font-bold uppercase hover:bg-red-600 hover:text-white hover:shadow-block hover:scale-105 active:scale-95 transition-all',
+        'solid' => 'inline-block bg-red-600 text-white border-2 border-black px-2 py-1 font-bold uppercase hover:bg-white hover:text-black hover:shadow-block hover:scale-105 active:scale-95 transition-all',
+        'outline' => 'inline-block bg-white text-black border-2 border-black px-2 py-1 font-bold uppercase hover:bg-red-600 hover:text-white hover:shadow-block hover:scale-105 active:scale-95 transition-all',
         'ghost' => 'text-white underline decoration-transparent hover:decoration-white hover:scale-105 active:scale-95 transition-all',
-        'danger' => 'font-bold uppercase px-4 py-1 bg-red-600 text-white border-2 border-black hover:bg-red-700 hover:scale-105 active:scale-95 transition-all',
-        'warning' => 'font-bold uppercase px-4 py-1 bg-yellow-600 text-white border-2 border-black hover:bg-yellow-700 hover:scale-105 active:scale-95 transition-all',
+        'danger' => 'font-bold uppercase px-2 py-1 bg-red-600 text-white border-2 border-black hover:bg-red-700 hover:scale-105 active:scale-95 transition-all',
+        'warning' => 'inline-block bg-yellow-500 text-white border-2 border-black px-2 py-1 font-bold uppercase hover:bg-white hover:text-black hover:shadow-block hover:scale-105 active:scale-95 transition-all',
         default => 'leading-relaxed text-blue-500 hover:underline'
     };
 @endphp
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,11 +9,11 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="antialiased min-h-screen flex flex-col bg-slate-50 text-slate-900">
+<body class="antialiased h-screen flex flex-col bg-slate-50 text-slate-900">
     <x-navbar />
 
     <div class="flex flex-1 overflow-hidden">
-        <aside class="w-64 border-r border-slate-200 overflow-y-auto bg-white/80 backdrop-blur hidden md:block h-full">
+        <aside class="w-64 border-r-2 border-black overflow-y-auto bg-gray-50 hidden md:block h-full">
             <x-sidebar />
         </aside>
 
