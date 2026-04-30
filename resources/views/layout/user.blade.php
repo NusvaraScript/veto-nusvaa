21c4d8a992bcaed9e139404edab4f90596523e43
Nusvara <yusufusman11111111@gmail.com>
2026-04-30 13:21:54 +0700

    yes

--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -7,7 +7,11 @@
     <title>@yield('title')</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    @if(app()->environment('local'))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+    <script src="https://cdn.tailwindcss.com"></script>
+    @endif
 </head>
 
 <body class="antialiased h-screen flex flex-col bg-white text-slate-900">
